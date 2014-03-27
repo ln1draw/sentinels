cb63c3be24f2b71838e37e33076df6ea8b6518f6
shanghaiellen <shanghaiellen@gmail.com>
2014-03-27 11:25:52 -0700

    activate/deactivate card works, albeit with repeated controller functions and without hitting the ajs model

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -30,6 +30,15 @@ myAppControllers.controller('HeroesController', ['$scope', '$http',
         $scope.hero = data;
       });
     };
+
+    $scope.deactivate_card = function(cardId){
+      // This should be a patch request
+      // Does AngularJS support patch?
+      $http.get('http://localhost:3000/cards/' + cardId + '/deactivate.json').success(function(data){
+        $scope.hero = data;
+      });
+    };
+
   }]);
 
 myAppControllers.controller('HeroController', ['$scope', '$routeParams', '$http',
@@ -37,6 +46,22 @@ myAppControllers.controller('HeroController', ['$scope', '$routeParams', '$http'
     $http.get('http://localhost:3000/heroes/' + $routeParams.heroId + '.json').success(function(data){
       $scope.hero = data;
     });
+
+    $scope.activate_hero = function(heroId){
+      // This should be a patch request
+      // Does AngularJS support patch?
+      $http.get('http://localhost:3000/heroes/' + heroId + '/activate.json').success(function(data){
+        $scope.hero = data;
+      });
+    };
+
+    $scope.deactivate_hero = function(heroId){
+      // This should be a patch request
+      // Does AngularJS support patch?
+      $http.get('http://localhost:3000/heroes/' + heroId + '/deactivate.json').success(function(data){
+        $scope.hero = data;
+      });
+    };
   }]);
 
 myAppControllers.controller("CardsController", ['$scope', '$http',
@@ -44,6 +69,22 @@ myAppControllers.controller("CardsController", ['$scope', '$http',
     $http.get('http://localhost:3000/cards/cards.json').success(function(data){
       $scope.cards = data;
     });
+
+    $scope.activate_card = function(cardId){
+      // This should be a patch request
+      // Does AngularJS support patch?
+      $http.get('http://localhost:3000/cards/' + cardId + '/activate.json').success(function(data){
+        $scope.hero = data;
+      });
+    };
+
+    $scope.deactivate_card = function(cardId){
+      // This should be a patch request
+      // Does AngularJS support patch?
+      $http.get('http://localhost:3000/cards/' + cardId + '/deactivate.json').success(function(data){
+        $scope.hero = data;
+      });
+    };
   }]);
 
 myAppControllers.controller('CardController', ['$scope', '$routeParams', '$http',
@@ -51,4 +92,20 @@ myAppControllers.controller('CardController', ['$scope', '$routeParams', '$http'
     $http.get('http://localhost:3000/cards/' + $routeParams.cardId + '.json').success(function(data){
       $scope.card = data;
     });
+
+    $scope.activate_card = function(cardId){
+      // This should be a patch request
+      // Does AngularJS support patch?
+      $http.get('http://localhost:3000/cards/' + cardId + '/activate.json').success(function(data){
+        $scope.hero = data;
+      });
+    };
+
+    $scope.deactivate_card = function(cardId){
+      // This should be a patch request
+      // Does AngularJS support patch?
+      $http.get('http://localhost:3000/cards/' + cardId + '/deactivate.json').success(function(data){
+        $scope.hero = data;
+      });
+    };
   }]);
